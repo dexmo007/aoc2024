6d2fd91da4078ab32c6d362da2774b6d224ca4e1
Henrik Drefs <henrik.drefs@outlook.de>
2024-12-07 03:48:45 +0100

    chore: generate new day script

---
+++ next_day.sh
@@ -0,0 +1,30 @@
+set -e
+set -e pipefail
+
+most_recent_day=$(ls -d src/*/ | sed -rn 's/^src\/day(.*)\/$/\1/p' | xargs -n1 | sort -r | head -1)
+
+new_day=$(($most_recent_day + 1))
+new_day=$(printf "%02d" $new_day)
+
+mkdir -p src/day$new_day
+touch src/day$new_day/sample.txt
+touch src/day$new_day/input.txt
+cat <<EOF > src/day$new_day/mod.rs
+
+use crate::aoc_core::{AocResult, AocTask};
+
+pub struct Day$new_day;
+
+impl AocTask for Day$new_day {
+
+    fn solve_a(&self, contents: String) -> AocResult {
+        todo!()
+    }
+
+    fn solve_b(&self, _contents: String) -> AocResult {
+        todo!()
+    }
+
+}
+
+EOF
