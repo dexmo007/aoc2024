995424c9716ba05d3796ccb221f95caa6ccf5cea
Henrik Drefs <henrik.drefs@outlook.de>
2024-12-09 23:44:53 +0100

    tried more stuff

--- src/day06/part_b.rs
+++ src/day06/part_b.rs
@@ -1,22 +1,27 @@
-use std::{ops::Shl, str::FromStr};
+use std::{
+    collections::{HashMap, HashSet},
+    ops::Shl,
+    str::FromStr,
+    time::{Duration, Instant},
+};
 
 use crate::aoc_core::AocResult;
 
-use super::core::{move_and_get, Direction as DirectionDelta, DOWN, LEFT, RIGHT, UP};
+use super::core::{Direction as DirectionDelta, DOWN, LEFT, RIGHT, UP};
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 enum PositionType {
+    Initial,
     Free,
-    Visited(u8),
     Obstruction,
 }
 impl FromStr for PositionType {
     type Err = String;
     fn from_str(value: &str) -> Result<Self, Self::Err> {
         match value {
+            "^" => Ok(Self::Initial),
             "." => Ok(Self::Free),
             "#" => Ok(Self::Obstruction),
-            "^" => Ok(Self::Visited(1 << Direction::Up)),
             _ => Err(String::from("invalid position type representation")),
         }
     }
@@ -65,93 +70,149 @@ impl Direction {
     }
 }
 
-fn mark_visited_with_loop(
-    map: &mut Vec<Vec<PositionType>>,
-    (mut y, mut x): (usize, usize),
-    mut direction: Direction,
-) -> bool {
+#[inline]
+pub fn move_guard(
+    (dim_y, dim_x): Dimensions,
+    (y, x): Position,
+    (dy, dx): DirectionDelta,
+) -> Option<(usize, usize)> {
+    let ny = y.checked_add_signed(dy)?;
+    if ny >= dim_y {
+        return None;
+    }
+    let nx = x.checked_add_signed(dx)?;
+    if nx >= dim_x {
+        return None;
+    }
+    Some((ny, nx))
+}
+
+fn mark_visited_with_loop<'a>(
+    dimensions: Dimensions,
+    obstructions: &HashSet<Position>,
+    state: &State,
+) -> (bool, HashMap<Position, u8>) {
+    let State {
+        mut position,
+        mut direction,
+    } = state;
+    let mut visited = HashMap::new();
+    visited.insert(position, 1 << direction);
     loop {
-        match move_and_get(&map, y, x, direction.as_deltas()) {
-            None => return false,
-            Some((ny, nx, PositionType::Visited(mask))) => {
-                if (mask & (1 << direction)) > 0 {
-                    return true;
+        match move_guard(dimensions, position, direction.as_deltas()) {
+            None => return (false, visited),
+            Some((ny, nx)) => match visited.get_mut(&(ny, nx)) {
+                Some(mask) => {
+                    if *mask & (1 << direction) > 0 {
+                        return (true, visited);
+                    }
+
+                    *mask |= 1 << direction;
+
+                    position = (ny, nx);
                 }
-                map[ny][nx] = PositionType::Visited(mask | (1 << direction));
-                (y, x) = (ny, nx);
-            }
-            Some((_, _, PositionType::Obstruction)) => {
-                direction = direction.turn_90_deg();
-            }
-            Some((ny, nx, PositionType::Free)) => {
-                map[ny][nx] = PositionType::Visited(1 << direction);
-                (y, x) = (ny, nx);
-            }
+                None => {
+                    if obstructions.contains(&(ny, nx)) {
+                        direction = direction.turn_90_deg();
+                        continue;
+                    }
+
+                    visited.insert((ny, nx), 1 << direction);
+                    position = (ny, nx);
+                }
+            },
         }
     }
 }
 
-struct Input {
-    map: Vec<Vec<PositionType>>,
-    position: (usize, usize),
+type Position = (usize, usize);
+type Dimensions = (usize, usize);
+
+struct State {
+    position: Position,
     direction: Direction,
 }
+struct Grid {
+    dimensions: Dimensions,
+    obstructions: HashSet<Position>,
+}
 
-fn parse_input(contents: String) -> Result<Input, String> {
-    let mut map = Vec::new();
+fn parse_input(contents: String) -> Result<(Grid, State), String> {
+    let mut obstructions = HashSet::new();
     let mut position = None;
+    let mut dim_y = 0;
+    let mut dim_x = None;
     for (y, line) in contents.lines().enumerate() {
-        let mut row = Vec::new();
+        dim_y += 1;
+        if dim_x.is_none() {
+            dim_x = Some(line.len());
+        }
         for (x, c) in line.chars().enumerate() {
             let position_type = PositionType::from_str(&c.to_string())?;
             match position_type {
-                PositionType::Visited(_) => {
+                PositionType::Initial => {
                     position = Some((y, x));
                 }
-                _ => (),
+                PositionType::Obstruction => {
+                    obstructions.insert((y, x));
+                }
+                PositionType::Free => (),
             }
-            row.push(position_type);
         }
-        map.push(row);
     }
-    let position = position.ok_or(String::from("no starting position found"))?;
-    Ok(Input {
-        map,
-        position,
-        direction: Direction::Up,
-    })
+    let position = position.ok_or("no starting position found")?;
+    let dim_x = dim_x.ok_or("Empty grid")?;
+    Ok((
+        Grid {
+            dimensions: (dim_y, dim_x),
+            obstructions,
+        },
+        State {
+            position,
+            direction: Direction::Up,
+        },
+    ))
 }
 
 pub fn solve_b(contents: String) -> AocResult {
-    let Input {
-        mut map,
-        direction,
-        position: starting_position,
-    } = parse_input(contents)?;
-    let original_map = map.to_vec();
-    let already_looped = mark_visited_with_loop(&mut map, starting_position, direction);
-    if already_looped {
+    let (grid, initial_state) = parse_input(contents)?;
+
+    let (looped, visited) =
+        mark_visited_with_loop(grid.dimensions, &grid.obstructions, &initial_state);
+    if looped {
         return Err(String::from("Guard is already looping"));
     }
 
-    let possible_obstruction_positions = map.iter().enumerate().flat_map(|(y, row)| {
-        row.iter().enumerate().filter_map(move |(x, p)| match *p {
-            PositionType::Visited(_) if (y, x) != starting_position => Some((y, x)),
-            _ => None,
-        })
-    });
+    let possible_obstruction_positions = visited
+        .keys()
+        .map(|&pos| pos)
+        .filter(|&pos| pos != initial_state.position);
 
+    let mut count = 0;
     let mut looping_obstruction_positions = 0;
+    let mut previous_obstruction: Option<(usize, usize)> = None;
+    let mut obstructions = grid.obstructions.clone();
+    let mut duration = Vec::new();
     for (oy, ox) in possible_obstruction_positions {
-        let mut map = original_map.to_vec();
-        map[oy][ox] = PositionType::Obstruction;
+        count += 1;
+        if let Some(previous_obstruction) = previous_obstruction {
+            obstructions.remove(&previous_obstruction);
+        }
 
-        if mark_visited_with_loop(&mut map, starting_position, direction) {
+        obstructions.insert((oy, ox));
+        let before = Instant::now();
+        let (looped, _) = mark_visited_with_loop(grid.dimensions, &obstructions, &initial_state);
+        duration.push(before.elapsed());
+        if looped {
             looping_obstruction_positions += 1;
         }
+        previous_obstruction = Some((oy, ox))
     }
-
-    // TODO maybe faster to clean the existing vector
+    println!("{count}");
+    println!(
+        "{:?}",
+        duration.iter().sum::<Duration>() / duration.len() as u32
+    );
 
     Ok(looping_obstruction_positions)
 }
