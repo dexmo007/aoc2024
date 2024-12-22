1f0174e6aa295cfec1af7c5f3f582ea4faddd676
Henrik Drefs <henrik.drefs@outlook.de>
2024-12-22 15:19:10 +0100

    day 12 b

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 use crate::aoc_core::{AocResult, AocTask};
 
@@ -46,8 +46,45 @@ impl AocTask for Day12 {
         Ok(result)
     }
 
-    fn solve_b(&self, _contents: String) -> AocResult {
-        todo!()
+    fn solve_b(&self, contents: String) -> AocResult {
+        let map: Vec<Vec<_>> = contents.lines().map(|l| l.chars().collect()).collect();
+
+        let mut regions = Vec::new();
+
+        let mut seen = HashSet::new();
+
+        let mut queue = map
+            .iter()
+            .enumerate()
+            .flat_map(|(y, row)| row.iter().enumerate().map(move |(x, _)| (y, x)))
+            .collect::<HashSet<_>>();
+
+        while !queue.is_empty() {
+            let next = *queue.iter().next().unwrap();
+            queue.remove(&next);
+            if seen.contains(&next) {
+                continue;
+            }
+
+            let mut region = HashSet::new();
+            scan_for_region(&map, &mut region, next);
+
+            for pos in &region {
+                seen.insert(*pos);
+            }
+            regions.push(region);
+        }
+        // for region in regions {
+        //     println!("{:?}", region);
+        // }
+        let mut result = 0i64;
+        for region in regions {
+            let m = measure_region_b(&region);
+            // println!("{:?}", region);
+            // println!("{:?}", m);
+            result += m.area as i64 * m.sides as i64;
+        }
+        Ok(result)
     }
 }
 
@@ -100,5 +137,63 @@ fn scan_for_region(map: &Vec<Vec<char>>, region: &mut HashSet<Position>, positio
 
     if x < row.len() - 1 && row[x + 1] == plant {
         scan_for_region(map, region, (y, x + 1));
+    }
+}
+
+#[derive(Debug)]
+struct RegionMeasurementB {
+    area: usize,
+    sides: u32,
+}
+
+fn measure_region_b(region: &HashSet<Position>) -> RegionMeasurementB {
+    let mut hsides = BTreeSet::new();
+    let mut vsides = BTreeSet::new();
+    for &(y, x) in region {
+        if y == 0 || !region.contains(&(y - 1, x)) {
+            hsides.insert((y, x, 0));
+        }
+        if !region.contains(&(y, x + 1)) {
+            vsides.insert((x + 1, y, 1));
+        }
+        if !region.contains(&(y + 1, x)) {
+            hsides.insert((y + 1, x, 1));
+        }
+        if x == 0 || !region.contains(&(y, x - 1)) {
+            vsides.insert((x, y, 0));
+        }
+    }
+    let mut sides = 0u32;
+    let mut last = None;
+    for &(y, x, side) in hsides.iter() {
+        match last {
+            None => {
+                sides += 1;
+            }
+            Some((ly, lx, lside)) => {
+                if y != ly || x > lx + 1 || side != lside {
+                    sides += 1;
+                }
+            }
+        }
+        last = Some((y, x, side));
+    }
+    let mut last = None;
+    for (x, y, side) in vsides {
+        match last {
+            None => {
+                sides += 1;
+            }
+            Some((lx, ly, lside)) => {
+                if x != lx || y > ly + 1 || side != lside {
+                    sides += 1;
+                }
+            }
+        }
+        last = Some((x, y, side));
+    }
+    RegionMeasurementB {
+        area: region.len(),
+        sides,
     }
 }
